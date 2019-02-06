628a4ef028051d728a6991635fa8d8a42210fd0a
Tin Wong <wongt1@wit.edu>
2019-02-06 16:49:24 -0500

    refactored fragment and layout
    changed RestaurantInfo to Place and RestaurantSelection to Selection (Removed Restaurant since it's not limited to just Restaurants)

--- app/src/main/java/com/example/whim/PlaceFragment.java
+++ app/src/main/java/com/example/whim/PlaceFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link RestaurantInfoFragment.OnFragmentInteractionListener} interface
+ * {@link PlaceFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link RestaurantInfoFragment#newInstance} factory method to
+ * Use the {@link PlaceFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RestaurantInfoFragment extends Fragment {
+public class PlaceFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class RestaurantInfoFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public RestaurantInfoFragment() {
+    public PlaceFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class RestaurantInfoFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RestaurantInfoFragment.
+     * @return A new instance of fragment PlaceFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RestaurantInfoFragment newInstance(String param1, String param2) {
-        RestaurantInfoFragment fragment = new RestaurantInfoFragment();
+    public static PlaceFragment newInstance(String param1, String param2) {
+        PlaceFragment fragment = new PlaceFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class RestaurantInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant, container, false);
+        return inflater.inflate(R.layout.fragment_place, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/whim/SelectionFragment.java
+++ app/src/main/java/com/example/whim/SelectionFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link RestaurantSelectionFragment.OnFragmentInteractionListener} interface
+ * {@link SelectionFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link RestaurantSelectionFragment#newInstance} factory method to
+ * Use the {@link SelectionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RestaurantSelectionFragment extends Fragment {
+public class SelectionFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class RestaurantSelectionFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public RestaurantSelectionFragment() {
+    public SelectionFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class RestaurantSelectionFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RestaurantSelectionFragment.
+     * @return A new instance of fragment SelectionFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RestaurantSelectionFragment newInstance(String param1, String param2) {
-        RestaurantSelectionFragment fragment = new RestaurantSelectionFragment();
+    public static SelectionFragment newInstance(String param1, String param2) {
+        SelectionFragment fragment = new SelectionFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class RestaurantSelectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_selection, container, false);
+        return inflater.inflate(R.layout.fragment_selection, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
