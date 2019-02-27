49f39d9cfbcfefba9495e9d6df633bae65d7f0bc
Tin Wong <wongt1@wit.edu>
2019-02-27 17:23:20 -0500

    created WhimDatabase and WhimDatabaseHelper

---
+++ app/src/main/java/com/example/whim/WhimDatabase.java
@@ -0,0 +1,10 @@
+package com.example.whim;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class WhimDatabase
+{
+    private WhimDatabaseHelper dbHelper;
+
+    private SQLiteDatabase database;
+}
