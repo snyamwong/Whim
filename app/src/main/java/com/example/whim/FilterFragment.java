4de5fbf1f58dfbb5d94e21a3f8ae7ecf0261ad5d
Tin Wong <wongt1@wit.edu>
2019-03-17 20:38:43 -0400

    added sql methods for insert and getAll

--- app/src/main/java/com/example/whim/FilterFragment.java
+++ app/src/main/java/com/example/whim/FilterFragment.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,12 +17,14 @@ import android.view.ViewGroup;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.example.whim.MainActivity.locationManager;
+import static android.content.Context.LOCATION_SERVICE;
 
 public class FilterFragment extends Fragment
 {
 
     private OnFragmentInteractionListener mListener;
+
+    private LocationManager locationManager;
 
     public FilterFragment()
     {
@@ -76,7 +79,51 @@ public class FilterFragment extends Fragment
         mListener = null;
     }
 
-    private Map<String, String> bundleFields()
+    private void createLocationManager()
+    {
+        LocationManager locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener()
+        {
+            @Override
+            public void onLocationChanged(Location location)
+            {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras)
+            {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider)
+            {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider)
+            {
+
+            }
+        });
+    }
+
+    private Map<String, String> getFields()
     {
         Map<String, String> fields = new HashMap<>();
 
--- app/src/main/java/com/example/whim/WhimDatabaseHelper.java
+++ app/src/main/java/com/example/whim/WhimDatabaseHelper.java
@@ -1,13 +1,34 @@
 package com.example.whim;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.yelp.fusion.client.models.Business;
+import com.yelp.fusion.client.models.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WhimDatabaseHelper extends SQLiteOpenHelper
 {
-    private static final String DATABASE_NAME = "WhimDatabase";
+    private static final String DATABASE_NAME = "WhimDatabase.db";
     private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "business_table";
+
+    private static final String COL_1 = "ID";
+    private static final String COL_2 = "NAME";
+    private static final String COL_3 = "RATING";
+    private static final String COL_4 = "PRICE";
+    private static final String COL_5 = "CITY";
+    private static final String COL_6 = "STATE";
+    private static final String COL_7 = "ADDRESS";
+    private static final String COL_8 = "ZIP_CODE";
+    private static final String COL_9 = "IMAGE";
+    private static final String[] COLUMNS = new String[]{COL_1, COL_2, COL_3, COL_4, COL_5, COL_6, COL_7, COL_8, COL_9};
 
     public WhimDatabaseHelper(Context context)
     {
@@ -17,12 +38,78 @@ public class WhimDatabaseHelper extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db)
     {
+        String createTable = String.format("create table if not exists %s " +
+                "(ID PRIMARY KEY TEXT, " +
+                "NAME TEXT, " +
+                "RATING FLOAT, " +
+                "PRICE TEXT, " +
+                "CITY TEXT, " +
+                "STATE TEXT, " +
+                "ADDRESS TEXT, " +
+                "ZIP_CODE INT," +
+                "IMAGE TEXT)", TABLE_NAME);
 
+        db.execSQL(createTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
 
+    public boolean insertData(Business business)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_1, business.getId());
+        values.put(COL_2, business.getName());
+        values.put(COL_3, business.getRating());
+        values.put(COL_4, business.getPrice());
+        values.put(COL_5, business.getLocation().getCity());
+        values.put(COL_6, business.getLocation().getState());
+        values.put(COL_7, business.getLocation().getAddress1());
+        values.put(COL_8, business.getLocation().getZipCode());
+        values.put(COL_9, business.getImageUrl());
+
+        long result = db.insert(TABLE_NAME, null, values);
+
+        return result != -1;
+    }
+
+    public List<Business> getAllData()
+    {
+        List<Business> result = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, COLUMNS, "*", null, null, null, null);
+        cursor.moveToFirst();
+
+        while(cursor.moveToNext())
+        {
+            Business business = new Business();
+            business.setId(cursor.getString(0));
+            business.setName(cursor.getString(1));
+            business.setRating(cursor.getDouble(2));
+            business.setPrice(cursor.getString(3));
+
+            Location location = new Location();
+            location.setCity(cursor.getString(5));
+            location.setState(cursor.getString(6));
+            location.setAddress1(cursor.getString(7));
+            location.setZipCode(cursor.getString(8));
+            business.setLocation(location);
+
+            business.setImageUrl(cursor.getString(9));
+
+            result.add(business);
+        }
+
+        cursor.close();
+        db.close();
+
+        return result;
     }
 }
