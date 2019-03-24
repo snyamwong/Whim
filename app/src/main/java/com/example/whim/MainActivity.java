8780d61397269aee45f865756725c56117eeb45c
Tin Wong <wongt1@wit.edu>
2019-03-24 13:23:07 -0400

    added a method to hide tab layout
    used for when switching to fragments that aren't search or favorite

--- app/src/main/java/com/example/whim/MainActivity.java
+++ app/src/main/java/com/example/whim/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.whim;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -17,9 +10,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements
         FavoriteFragment.OnFragmentInteractionListener,
@@ -40,36 +33,62 @@ public class MainActivity extends AppCompatActivity implements
 
         setContentView(R.layout.activity_main);
 
-        // Commented here for a quick switch to FilterFragment
-        /*
-        Fragment newFragment = new FilterFragment();
+        Fragment searchFragment = new SearchFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.main_content, newFragment).commit();
-        */
+        transaction.add(R.id.main_content, searchFragment).commit();
 
-        displayTabLayout();
+        showSupportActionBar();
+        showTabLayout();
     }
 
-    private void displayTabLayout()
+    /**
+     *
+     */
+    private void showSupportActionBar()
     {
-        setContentView(R.layout.activity_main);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().show();
+    }
 
+    /**
+     *
+     */
+    private void hideSupportActionBar()
+    {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+    }
+
+    /**
+     *
+     */
+    private void showTabLayout()
+    {
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
         ViewPager mViewPager = findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        TabLayout tabLayout = findViewById(R.id.tabs);
-
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+    /**
+     *
+     */
+    private void hideTabLayout()
+    {
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        tabLayout.setVisibility(View.GONE);
     }
 
     /**
